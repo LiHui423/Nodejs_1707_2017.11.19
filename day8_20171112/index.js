27e53e458bf03e9804bca2db7105d6a831547f94
thu <mingfei.net@gmail.com>
2017-11-12 17:40:46 +0800

    Update

--- day8_20171112/index.js
+++ day8_20171112/index.js
@@ -9,21 +9,41 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mysql = require('mysql');
 
 let app = express();
 
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(bodyParser.urlencoded());
+
+let pool = mysql.createPool({
+    connectionLimit: 10,
+    user: 'root'
+});
 
 app.get('/', (req, res) => { // get post put delete
     res.sendFile(__dirname + '/public/index.html');
 });
 
-app.post('/signIn', (req, res) => {
+app.get('/sign-up', (req, res) => { // get post put delete
+    res.sendFile(__dirname + '/public/sign-up.html');
+});
+
+app.post('/signUp', (req, res) => {
     let username = req.body.username;
     let password = req.body.password;
     res.send(username + ':' + password);
+    pool.getConnection((err, connection) => {
+        if(err) throw err;
+        connection.query('INSERT INTO db_demo.user VALUE(NULL, ?, ?)', [username, password], (err, results, fields) => {
+            console.log(results.affectedRows);
+            if (results.affectedRows == 1) {
+
+            } else {
+
+            }
+            connection.release();
+        })
+    })
 });
 
 app.listen(80);
