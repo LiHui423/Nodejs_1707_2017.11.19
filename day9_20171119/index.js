9d84d81fe41eef8c0ef4984640c2cbb7075db3a5
thu <mingfei.net@gmail.coom>
2017-11-19 17:04:00 +0800

    Update

--- day9_20171119/index.js
+++ day9_20171119/index.js
@@ -19,30 +19,57 @@ let pool = mysql.createPool({
 let app = express(); // 创建 express 对象
 
 app.use(bodyParser.urlencoded()); // 配置 body-parser 中间件，注意位置
+app.use(express.static(__dirname + '/public')); // 配置了静态目录 public
 
 app.get('/', (req, res) => { // 接受服务器的根目录 GET 请求
-   console.log('test...'); // 控制台输出信息
-   // res.end('end...'); // res:response end 方法在网页上输出字符串，同时结束了这次响应
+    console.log('test...'); // 控制台输出信息
+    // res.end('end...'); // res:response end 方法在网页上输出字符串，同时结束了这次响应
     res.sendFile(__dirname + '/public/index.html'); // 页面跳转
 });
-
-app.get('/sign-up', (req, res) => { // 来自于链接的请求
-    res.sendFile(__dirname + '/public/sign-up.html');
-}); // 更好的处理
 
 app.post('/signUp', (req, res) => { // 接受用户注册请求
     let username = req.body.username;
     let password = req.body.password;
 
     pool.getConnection((err, connection) => {
-        if(err) throw err;
-        let sql = 'INSERT INTO db_demo.user VALUE(NULL, ?, ?)';
-        connection.query(sql, [username, password], (err, results, fields) => {
-            if(err) throw err;
-            if (results.affectedRows === 1) { // 注册成功
-                res.sendFile(__dirname + '/public/index.html');
-            } else { // 注册失败
+        if (err) throw err;
+        let sql = 'SELECT * FROM db_demo.user WHERE username = ?';
+        connection.query(sql, [username], (err, results, fields) => {
+            if (err) throw err;
+            if (results.length === 1) {
                 res.sendFile(__dirname + '/public/sign-up.html');
+            } else {
+                pool.getConnection((err, connection) => {
+                    if (err) throw err;
+                    let sql = 'INSERT INTO db_demo.user VALUE(NULL, ?, ?)';
+                    connection.query(sql, [username, password], (err, results, fields) => {
+                        if (err) throw err;
+                        if (results.affectedRows === 1) { // 注册成功
+                            res.sendFile(__dirname + '/public/index.html');
+                        } else { // 注册失败
+                            res.sendFile(__dirname + '/public/sign-up.html');
+                        }
+                    });
+                    connection.release();
+                })
+            }
+        })
+    });
+});
+
+app.post('/signIn', (req, res) => {
+    let username = req.body.username;
+    let password = req.body.password;
+
+    pool.getConnection((err, connection) => {
+        if (err) throw err;
+        let sql = 'SELECT * FROM db_demo.user WHERE username = ? AND password = ?'; // Alt + Shift + U
+        connection.query(sql, [username, password], (err, results, fields) => {
+            if (err) throw err;
+            if (results.length === 1) {
+                res.sendFile(__dirname + '/public/home.html');
+            } else {
+                res.sendFile(__dirname + '/public/index.html');
             }
         });
         connection.release();
