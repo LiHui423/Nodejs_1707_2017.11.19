9eb65f1199c6d0627ae3541a5aa1697de57032f7
thu <mingfei.net@gmail.coom>
2017-11-19 10:41:03 +0800

    Update

--- day9_20171119/index.js
+++ day9_20171119/index.js
@@ -5,4 +5,14 @@
  * Released under the MIT license 
  * 
  * Date: 2017/11/19 10:16
- */  
+ */
+
+const express = require('express'); // 引入 express 模块
+
+let app = express(); // 创建 express 对象
+
+app.get('/', (req, res) => {
+   console.log('/...');
+});
+
+app.listen(80);
